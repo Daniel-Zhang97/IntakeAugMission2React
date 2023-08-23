a8c7217cdbcfca7229ee98c7ed1e50df6b2d1ab7
Zenzo1 <d.zhang9707@gmail.com>
2023-08-24 02:32:18 +1200

    fixed the tagname recognition system slightly and added generative responses

--- frontend/src/components/ImageUploader.js
+++ frontend/src/components/ImageUploader.js
@@ -1,47 +1,60 @@
-import React, { useState } from "react";
-import axios from "axios";
-import carsData from "./cars.json"; // Import your JSON data
+import React, { useState } from 'react'
+import axios from 'axios'
+import carsData from './cars.json' // Import your JSON data
 
 const ImageUploader = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [similarCar, setSimilarCar] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null)
+  const [similarCar, setSimilarCar] = useState(null)
 
   const handleImageChange = (event) => {
-    setSelectedImage(event.target.files[0]);
-  };
+    setSelectedImage(event.target.files[0])
+  }
 
   const handleImageUpload = async () => {
     try {
       if (!selectedImage) {
-        console.log("No image selected.");
-        return;
+        console.log('No image selected.')
+        return
       }
 
-      const formData = new FormData();
-      formData.append("image", selectedImage);
+      const formData = new FormData()
+      formData.append('image', selectedImage)
 
       const response = await axios.post(
-        "http://localhost:5000/api/upload",
+        'http://localhost:5000/api/upload',
         formData
-      );
+      )
 
-      console.log("API Response:", response.data);
+      console.log('API Response:', response.data)
 
-      const topPrediction = response.data.response.predictions[0];
+      const aiPredictions = response.data.response.predictions
+      const targetTags = aiPredictions.map((prediction) =>
+        prediction.tagName.toLowerCase()
+      )
 
+      // console.log('top prediction:', response.data.response.predictions[0])
+      let similarCars = []
       // Find the first similar car based on the top prediction
-      const similarCar = carsData.carsList.find(
-        (car) => car.model.toLowerCase() === topPrediction.tagName.toLowerCase()
-      );
+      for (let i = 0; i < Math.min(3, targetTags.length); i++) {
+        const filteredCars = carsData.carsList.filter(
+          (car) => car.model.toLowerCase() === targetTags[i]
+        )
 
-      console.log("Found Similar Car:", similarCar);
+        if (filteredCars.length > 0 && similarCars.length < 3) {
+          for (const car of filteredCars) {
+            similarCars.push(car)
+          }
+        }
+      }
+
+      console.log('Found Similar Car:', similarCars)
 
       // Update the similarCar state with the found similar car
-      setSimilarCar(similarCar);
+      setSimilarCar(similarCars)
     } catch (error) {
-      console.error("Error:", error);
+      console.error('Error:', error)
     }
-  };
+  }
 
   return (
     <div>
@@ -51,22 +64,21 @@ const ImageUploader = () => {
 
       {similarCar && (
         <div>
-          <h2>Similar Car</h2>
-          <div>
-            <h3>
-              {similarCar.brand} {similarCar.model}
-            </h3>
-            <p>Year: {similarCar.year}</p>
-            <p>Color: {similarCar.color}</p>
-            <img
-              src={similarCar.imageURL}
-              alt={`${similarCar.brand} ${similarCar.model}`}
-            />
-          </div>
+          <h2>Similar Cars</h2>
+          {similarCar.map((car, index) => (
+            <div key={index}>
+              <h3>
+                {car.brand} {car.model}
+              </h3>
+              <p>Year: {car.year}</p>
+              <p>Color: {car.color}</p>
+              <img src={car.imageURL} alt={`${car.brand} ${car.model}`} />
+            </div>
+          ))}
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default ImageUploader;
+export default ImageUploader
